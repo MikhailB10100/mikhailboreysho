71aa36fe9f62fb6333a33ae09ffc0709034757c7
Mikhail <73386949+MikhailB10100@users.noreply.github.com>
2021-08-09 06:29:29 +0500

    Create App-component core, update structure
    - Remove comments and base styles from template
    - Create Header and Footer components
    - Create Routing component (need add routes)
    - Add components to App-component and update it's own structure
    - Change primary div in index.html id from 'app' to 'root'

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Home from '@components/Home/Home'
+import Header from '@components/Header/Header'
+import Footer from '@components/Footer/Footer'
+import Routing from '@components/router/Routing'
+
 
 function App() {
   return (
-    <div className='content'>
-      <Home />
+    <div className='app'>
+      <Header />
+      <div className='content-body'>
+        <Routing />
+        <Footer />
+      </div>
     </div>
   )
 }
