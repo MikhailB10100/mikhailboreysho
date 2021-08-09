2e7bc4705ebf83c27a44cf1f6e93f556c9520e6e
Mikhail <73386949+MikhailB10100@users.noreply.github.com>
2021-08-09 05:33:35 +0500

    Add routing core
    - Create router
    - Add router to root index.js

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const router = require('./router/index')
 
 const PORT = process.env.PORT || 5000
 const app = express()
@@ -11,6 +12,7 @@ app.use(cors({
   credentials: true,
   origin: process.env.CLIENT_URL,
 }))
+app.use('/api', router)
 
 const start = async () => {
   try {
